9b831c19d2148ff38b1443f0a17f2318df16547d
denis <denis@denis-System-Product-Name>
2013-08-08 13:15:42 +0200

    Denis: Database klasea gehitu. Klase honek datubasea jaitsiko du behar izanez gero, md5 ta konparatuz, ta gero irakurketak eta idazketak egiteko funtzioak eskainiko ditu aplikazioari

---
+++ DataBaseMD5Download.cpp
@@ -0,0 +1,18 @@
+/* 
+ * File:   DataBaseMD5Download.cpp
+ * Author: denis
+ * 
+ * Created on August 8, 2013, 1:13 PM
+ */
+
+#include "DataBaseMD5Download.h"
+
+DataBaseMD5Download::DataBaseMD5Download() {
+}
+
+DataBaseMD5Download::DataBaseMD5Download(const DataBaseMD5Download& orig) {
+}
+
+DataBaseMD5Download::~DataBaseMD5Download() {
+}
+
---
+++ DataBaseMD5Download.h
@@ -0,0 +1,21 @@
+/* 
+ * File:   DataBaseMD5Download.h
+ * Author: denis
+ *
+ * Created on August 8, 2013, 1:13 PM
+ */
+
+#ifndef DATABASEMD5DOWNLOAD_H
+#define	DATABASEMD5DOWNLOAD_H
+
+class DataBaseMD5Download {
+public:
+    DataBaseMD5Download();
+    DataBaseMD5Download(const DataBaseMD5Download& orig);
+    virtual ~DataBaseMD5Download();
+private:
+
+};
+
+#endif	/* DATABASEMD5DOWNLOAD_H */
+
